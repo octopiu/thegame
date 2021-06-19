ff0f69c199a6d4bdca45dd59b3967af46b82b88a
a.drozhevskij <a.drozhevskij@timeweb.ru>
2021-06-20 02:36:47 +0300

    rename docs to html in run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ fi
 docker -D run --rm --name thegame -d \
 	-v ${PWD}/thegame.conf:/etc/nginx/conf.d/default.conf \
 	-v ${PWD}/error.log:/var/log/nginx/host.error.log \
-	-v ${PWD}/docs:/usr/share/nginx/html \
+	-v ${PWD}/html:/usr/share/nginx/html \
 	-p 80:80 \
 	nginx:alpine
